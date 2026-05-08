a2ca6b75d16e74ecc4c2ee10859d9a70653e7c18
NandoAgueroS <nandoaguerojs@gmail.com>
2026-05-08 16:24:59 -0300

    fix: convertir loginService a objeto

--- ConAlambre-Front/src/services/loginService.ts
+++ ConAlambre-Front/src/services/loginService.ts
@@ -1,10 +1,6 @@
 import { api } from "../client/api";
 import type { LoginRequest, LoginResponse } from "../types/auth";
 
-export const loginService = async (
-  request: LoginRequest,
-): Promise<LoginResponse> => {
-  const { data } = await api.post<LoginResponse>("/login", request);
-  return data;
+export const loginService = {
+  login: (request: LoginRequest) => api.post<LoginResponse>("/login", request),
 };
-
